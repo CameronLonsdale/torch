4c561bc9440147a168930cf530bf7bcf6c830a16
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-05-05 22:20:38 +1000

    verbosity for printing keys and score key option for decrypting instead of cracking improved output formatting

--- torch.py
+++ torch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+import sys
 
 import click
 
@@ -8,34 +8,63 @@ from lantern import (
 
 
 @click.group()
+@click.option('-n', default=1)
+@click.option('-v', '--verbose', is_flag=True)
 @click.pass_context
-def cli(ctx):
-    ctx.obj = {}
-    ctx.obj['CIPHERTEXT'] = str(click.prompt('', prompt_suffix='', type=str))
+def cli(ctx, n, verbose):
+    ciphertext = str(sys.stdin.read()).rstrip()
+    ctx.obj = {
+        'CIPHERTEXT': ciphertext,
+        'LIMIT': n,
+        'SPACE_OUTPUT': ciphertext.count('\n') > 1,
+        'VERBOSE': verbose
+    }
+
+
+def output(decryptions, ctx):
+    verbose = ctx.obj['VERBOSE']
+
+    for decrypt in decryptions[:ctx.obj['LIMIT']]:
+        click.echo(format_output(decrypt, verbose))
+        if (ctx.obj['SPACE_OUTPUT'] or verbose):
+            click.echo()
+
+
+def format_output(decryption, verbose):
+    if verbose:
+        return "{0}\nkey: {1}\nscore: {2}".format(
+            decryption.plaintext, decryption.key, decryption.score
+        )
+    return decryption.plaintext
 
 
 @cli.command()
+@click.option('-k', '--key', default=None)
 @click.pass_context
-def auto(ctx):
-    click.echo('auto currently unsupported')
+def caesar(ctx, key):
+    ciphertext = ctx.obj['CIPHERTEXT']
+    if key:
+        click.echo(modules.caesar.decrypt(key, ciphertext))
+    else:
+        decryptions = modules.caesar.crack(
+            ciphertext, fitness.english.quadgrams
+        )
+        output(decryptions, ctx)
 
 
 @cli.command()
+@click.option('-k', '--key', default=None)
+@click.option('--ntrials', default=15)
 @click.pass_context
-def caesar(ctx):
-    decryptions = modules.caesar.crack(
-        ctx.obj['CIPHERTEXT'], fitness.english.quadgrams
-    )
-    click.echo(decryptions[0])
-
-
-@cli.command()
-@click.pass_context
-def substitution(ctx):
-    decryptions = modules.simplesubstitution.crack(
-        ctx.obj['CIPHERTEXT'], fitness.english.quadgrams
-    )
-    click.echo(decryptions[0])
+def substitution(ctx, key, ntrials):
+    ciphertext = ctx.obj['CIPHERTEXT']
+    if key:
+        click.echo(modules.simplesubstitution.decrypt(key, ciphertext))
+    else:
+        decryptions = modules.simplesubstitution.crack(
+            ciphertext, fitness.english.quadgrams, ntrials
+        )
+        output(decryptions, ctx)
 
 
 @cli.command()
@@ -46,8 +75,4 @@ def vigenere(ctx):
         fitness.ChiSquared(analysis.frequency.english.quadgrams),
         max_key_period=30
     )
-    click.echo(decryptions[0])
-
-
-if __name__ == '__main__':
-    cli(obj={})
+    output(decryptions, ctx)
