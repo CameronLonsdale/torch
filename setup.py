e30164ec8841d050d7c7b4501f7fdf96d91e38f5
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-06-29 17:49:09 +1000

    Updating setup.py with more project metadata

--- setup.py
+++ setup.py
@@ -5,9 +5,14 @@ from setuptools import setup
 setup(
     name="torch-crypto",
     version='0.1.0',
+    description="Command-line Cryptanalysis",
+    author='Cameron Lonsdale',
+    author_email='cameron.lonsdale@gmail.com',
+    url='https://github.com/CameronLonsdale/torch',
+    license='MIT',
     install_requires=['click', 'lantern'],
     scripts=['torch.py'],
     entry_points={
         'console_scripts': ['torch=torch:cli']
-    },
+    }
 )
