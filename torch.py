bcd1bb64fccc227fdc57fe3589cb319f3717b44e
Cameron Lonsdale <c.lonsdale@student.unsw.edu.au>
2017-06-29 17:44:24 +1000

    Small changes to conform to changed lantern API Small style changes

--- torch.py
+++ torch.py
@@ -1,25 +1,20 @@
+"""Command-line Cryptanalysis"""
+
 import sys
 
 import click
 
-from lantern import (
-    analysis, modules, fitness
-)
+from lantern import analysis, modules, fitness
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.option('-n', default=1, 
-    help="The upper limit on the number of possible decryptions to be printed")
-@click.option('-v', '--verbose', is_flag=True,
-    help="Additionally print out the key and score for each decryption")
+@click.option('-n', default=1, help="The upper limit on the number of possible decryptions to be printed")
+@click.option('-v', '--verbose', is_flag=True, help="Additionally print out the key and score for each decryption")
 @click.pass_context
 def cli(ctx, n, verbose):
-    """
-    Command-line Cryptanalysis.
-
-    Easily crack and decrypt classic ciphers from a terminal.
-    """
+    """Command-line Cryptanalysis."""
     ctx.obj = {
         'LIMIT': n,
         'VERBOSE': verbose
@@ -46,61 +41,57 @@ def output(decryptions, ctx):
 def format_output(decryption, verbose):
     """Return the formatted output based on verbosity"""
     if verbose:
-        return "{0}\nkey: {1}\nscore: {2}".format(
-            decryption.plaintext, decryption.key, decryption.score
-        )
+        return "{0}\nkey: {1}\nscore: {2}".format(decryption.plaintext, decryption.key, decryption.score)
     return decryption.plaintext
 
 
 @cli.command()
-@click.option('-k', '--key', default=None, type=int,
-    help="Use this key to decrypt")
+@click.option('-k', '--key', default=None, type=int, help="Use this key to decrypt")
 @click.pass_context
 def caesar(ctx, key):
     """Decrypt or crack a caesar cipher"""
     ciphertext, ctx = read_input(ctx)
     if key:
-        click.echo(modules.caesar.decrypt(key, ciphertext))
-    else:
-        decryptions = modules.caesar.crack(
-            ciphertext, fitness.english.quadgrams
-        )
-        output(decryptions, ctx)
+        return click.echo(modules.caesar.decrypt(key, ciphertext))
+
+    decryptions = modules.caesar.crack(
+        ciphertext,
+        fitness.english.quadgrams
+    )
+    output(decryptions, ctx)
 
 
 @cli.command()
-@click.option('-k', '--key', default=None, type=str,
-    help="Use this key to decrypt")
-@click.option('--ntrials', default=15, type=int,
-    help="Number of trials to run the substitution cipher cracker with")
+@click.option('-k', '--key', default=None, type=str, help="Use this key to decrypt")
+@click.option('--ntrials', default=15, type=int, help="Number of trials to run the substitution cipher cracker with")
 @click.pass_context
 def substitution(ctx, key, ntrials):
     """Decrypt or crack a simple substitution cipher"""
     ciphertext, ctx = read_input(ctx)
     if key:
-        click.echo(modules.simplesubstitution.decrypt(key, ciphertext))
-    else:
-        decryptions = modules.simplesubstitution.crack(
-            ciphertext, fitness.english.quadgrams, ntrials
-        )
-        output(decryptions, ctx)
+        return click.echo(modules.simplesubstitution.decrypt(key, ciphertext))
+
+    decryptions = modules.simplesubstitution.crack(
+        ciphertext,
+        fitness.english.quadgrams,
+        ntrials=ntrials
+    )
+    output(decryptions, ctx)
 
 
 @cli.command()
-@click.option('-k', '--key', default=None, type=str,
-    help="Use this key to decrypt")
-@click.option('-p', '--period', default=None, type=str,
-    help="Use this key to decrypt")
+@click.option('-k', '--key', default=None, type=str, help="Use this key to decrypt")
+@click.option('-p', '--period', default=None, type=int, help="Use this key to decrypt")
 @click.pass_context
 def vigenere(ctx, key, period):
     """Decrypt or crack a vigenere cipher"""
     ciphertext, ctx = read_input(ctx)
     if key:
-        click.echo(modules.vigenere.decrypt(key, ciphertext))
-    else:
-        decryptions = modules.vigenere.crack(
-            ciphertext,
-            fitness.ChiSquared(analysis.frequency.english.unigrams),
-            key_period=period
-        )
-        output(decryptions, ctx)
+        return click.echo(modules.vigenere.decrypt(key, ciphertext))
+
+    decryptions = modules.vigenere.crack(
+        ciphertext,
+        fitness.ChiSquared(analysis.frequency.english.unigrams),
+        key_period=period
+    )
+    output(decryptions, ctx)
